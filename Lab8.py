2b2eb22129e36c6d65ce99ff6bfbb2037392b0e2
Cristian R-C <31358896+Dabbingonmyhaters@users.noreply.github.com>
2020-04-30 14:05:29 -0700

    Update Lab8.py
    The basis for the code, it adds right now but not completely correct.

--- Lab8.py
+++ Lab8.py
@@ -1,1 +1,29 @@
+#day 1 progres
+def fibonacci_slow(n):
+    if n == 1:
+        return 1
+    #base case for fib numbers
+    elif n == 2:
+        return 1 
+    #second base case for fib numbers
+    else:
+        return fibonacci_slow(n-2) + fibonacci_slow(n-1)
+    #finds the values of any fib number after the third fib number
+
+def fib_even(x):
+    fibonacci_slow(x)<400000
+    #set the limit incase the function exceeds 4 million
+    total_sum = 0
+    #starting value to begin adding
+    for i in range(1, x):
+        if (i%2 == 0):
+            total_sum = total_sum+i
+    #essentially a for lup to continually add to get the sum of the even numbers. 
+    print(total_sum)
+    return total_sum
+
+
+fib_even(5)
+
+
 
